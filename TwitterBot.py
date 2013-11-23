e4e839854dead05e74138afa3c682b5850c76ae0
Brian Charous <brian.charous@gmail.com>
2013-11-22 23:35:59 -0600

    Added logging and some error checkign

--- TwitterBot.py
+++ TwitterBot.py
@@ -5,6 +5,7 @@ import random
 import twitter
 from time import sleep
 import sys
+import logging
 
 class TwitterBot:
 
@@ -27,19 +28,25 @@ class TwitterBot:
 			try:
 				return HeadlineGenerator.generateHeadlines(topic, self.model)
 			except HeadlineGenerator.BadSeedException:
-				print topic + " not in corpus"
+				logging.info("%s not in corpus", topic)
 				topics.remove(topic)
+			if not topics:
+				logging.info("no trending topics in corpus")
+				return None
 
 	def postToTwitter(self):
 
 		headline = self.generateHeadline()
-		print headline
-		self.twitter.PostUpdate(headline)
+		if headline:
+			logging.info("posting '%s'" % headline)
+			self.twitter.PostUpdate(headline)
 
 	def __init__(self, modelFilename):
 		self.twitter = twitter.Api(consumer_key = 'Xdfb5x3sYS2Q8E7nYD6fw', consumer_secret = 'gETgfldkzFG1BpFVLcwa0pOo1V3P9vovh4MRAcl4s', access_token_key = '2210011554-xfUSAyljGxFt2h11uCQqOQuUA3UR8ZRkQJYRTNN', access_token_secret = 'StBOKpJZCUwmop63wvlnqFU4EUNPYE5ZSm1i4z3Gu6ZXG')
+		logging.info("reading %s" modelFilename)
 		self.model = HeadlineGenerator.readLanguageModel(modelFilename)
-
+		logging.info("done reading model")
+		logging.basicConfig(filename = 'twitter_status.log', level = logging.INFO)
 
 def main():
 	modelname = sys.argv[1]
